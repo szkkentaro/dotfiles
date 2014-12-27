24cfb6b85c3df8fb870c514b17d08583304b4e73
szkkentaro <szk.kentaro@gmail.com>
2014-12-28 07:18:05 +0900

    create backup file

--- init.sh
+++ init.sh
@@ -4,9 +4,11 @@ FILE_LIST=("bashrc" "bash_profile" "gitconfig" "vimrc")
 
 for FILE in ${FILE_LIST[@]}
 do
-  if [ ! -e $HOME/.$FILE ]; then
-    CMD="ln -s $SCRIPT_DIR/$FILE $HOME/.$FILE"
+  if [ -e $HOME/.$FILE ]; then
+    CMD="mv $HOME/.$FILE{,.`date +%Y%m%d%H%M%S`}"
     `$CMD` && echo "$CMD"
   fi
+  CMD="ln -s $SCRIPT_DIR/$FILE $HOME/.$FILE"
+  `$CMD` && echo "$CMD"
 done
 echo "Done!"
