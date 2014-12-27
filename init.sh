5b385891f43b9ffe1fd6a4e39dbceb314337d5f9
szkkentaro <szk.kentaro@gmail.com>
2014-12-28 07:25:53 +0900

    bugfix

--- init.sh
+++ init.sh
@@ -5,8 +5,7 @@ FILE_LIST=("bashrc" "bash_profile" "gitconfig" "vimrc")
 for FILE in ${FILE_LIST[@]}
 do
   if [ -e $HOME/.$FILE ]; then
-    CMD="mv $HOME/.$FILE{,.`date +%Y%m%d%H%M%S`}"
-    `$CMD` && echo "$CMD"
+    mv $HOME/.$FILE{,.`date +%Y%m%d%H%M%S`}
   fi
   CMD="ln -s $SCRIPT_DIR/$FILE $HOME/.$FILE"
   `$CMD` && echo "$CMD"
