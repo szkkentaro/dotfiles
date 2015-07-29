85f55ad68c525dded7461cb4aedb744caedff2f8
Kentaro SUZUKI <szk.kentaro@gmail.com>
2015-07-30 00:53:58 +0900

    Modify init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
-FILE_LIST=("bashrc" "bash_profile" "gitconfig" "vimrc")
 
+# Download dotfiles
+DOTFILES=$HOME/.dotfiles
+git clone https://github.com/szkkentaro/dotfiles.git $DOTFILES
+
+# Expand dotfiles
+FILE_LIST=("bashrc" "bash_profile" "gitconfig" "vimrc")
 for FILE in ${FILE_LIST[@]}
 do
   if [ -e $HOME/.$FILE ]; then
     mv $HOME/.$FILE{,.`date +%Y%m%d%H%M%S`}
   fi
-  CMD="ln -s $SCRIPT_DIR/$FILE $HOME/.$FILE"
-  `$CMD` && echo "$CMD"
+  CMD="ln -s $DOTFILES/$FILE $HOME/.$FILE"
+  tee $CMD
 done
-echo "Done!"
+
+# Download .Vundle
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+# Download Powerline.otf
+curl -sL 'https://github.com/Lokaltog/powerline-fonts/blob/master/DroidSansMono/Droid%20Sans%20Mono%20for%20Powerline.otf?raw=true' -o ~/Library/Fonts/Droid\ Sans\ Mono\ for\ Powerline.otf
+
+# Vim PluginInstall
+vim +PluginInstall +qall
+
+# Vim Colors
+mkdir -p ~/.vim/colors
+
+# Vim Color Scheme
+ln -s ~/.vim/bundle/landscape.vim/colors/landscape.vim ~/.vim/colors/landscape.vim
